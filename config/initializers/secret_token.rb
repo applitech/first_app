d30073605f7d63f37fbc6d36c2e2d62b98b5d897
lionking <son.trinh.ky@gmail.com>
2014-02-26 13:25:29 +0700

    Initialize repositoryy from lion-king

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '64574cdc3eb427c4bb6df1577056520fa50786eca4227967679598069b2e0ff3bcfe42b6758aa32525b145954330565504b1757ce2f42e0766bd0045420e0572'
